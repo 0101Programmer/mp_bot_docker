6e415d960dff3a460cd489179234b653eadb8c50
Sergey Ermakov <seermakov355@yandex.ru>
2025-01-27 00:42:38 +0300

    Зарегистрированы модели в админке

--- telegram_bot/admin.py
+++ telegram_bot/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import User, CommissionInfo, Appeal, Notification
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'username')
+
+@admin.register(CommissionInfo)
+class CommissionInfoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+@admin.register(Appeal)
+class AppealAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'commission', 'status')
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'appeal', 'status', 'sent')
