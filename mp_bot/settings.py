10547f0c4e9a24f07af16de923b7f1260e6f3908
Sergey Ermakov <seermakov355@yandex.ru>
2025-01-27 19:05:14 +0300

    Исправление сигналов

--- mp_bot/settings.py
+++ mp_bot/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'telegram_bot',
+    'telegram_bot.apps.TelegramBotConfig',
 ]
 
 MIDDLEWARE = [
