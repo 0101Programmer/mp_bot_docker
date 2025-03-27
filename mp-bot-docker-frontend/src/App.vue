b965ad1e880d4eb2e887f8402d3fd6da25d718f3
Владимир <baranov-v-k@yandex.ru>
2025-03-27 21:25:47 +0300

    Подключение фронтенда к приложению

--- mp-bot-docker-frontend/src/App.vue
+++ mp-bot-docker-frontend/src/App.vue
@@ -3,28 +3,18 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+  <div class="min-h-screen bg-gray-100 flex flex-col items-center justify-center p-4">
+    <h1 class="text-4xl font-bold text-blue-600 mb-6">Vite + Vue + TailwindCSS</h1>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+    <div class="flex space-x-4">
+      <a href="https://vite.dev" target="_blank" class="hover:scale-110 transition-transform">
+        <img src="/vite.svg" alt="Vite logo" class="w-24 h-24 rounded-full shadow-lg" />
+      </a>
+      <a href="https://vuejs.org/" target="_blank" class="hover:scale-110 transition-transform">
+        <img src="./assets/vue.svg" alt="Vue logo" class="w-24 h-24 rounded-full shadow-lg" />
+      </a>
+    </div>
+
+    <HelloWorld msg="Vite + Vue" />
+  </div>
+</template>
