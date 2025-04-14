6ec5a38341b21dd33072f93a5f8b8ad1b08b00ef
Владимир <baranov-v-k@yandex.ru>
2025-04-14 21:14:47 +0300

    Добавлена проверка подключения к Redis (без использования Докера) при запуске бота через Джанго (для того, чтобы сразу видеть ошибки, связанные с ним)

--- telegram_bot/management/commands/runbot.py
+++ telegram_bot/management/commands/runbot.py
@@ -1,11 +1,35 @@
 import asyncio
 from django.core.management.base import BaseCommand
+from django_redis import get_redis_connection
+from decouple import config
 from telegram_bot.bot import start_bot
 
+USE_DOCKER = config("USE_DOCKER")
+
 class Command(BaseCommand):
-    help = 'Запуск Telegram-бота (aiogram 3.x)'
+    help = 'Запуск Telegram-бота'
+
+    def check_redis(self):
+        """
+        Проверяет доступность Redis.
+        В случае ошибки выводит сообщение и завершает выполнение команды.
+        """
+        try:
+            # Попытка подключиться к Redis
+            redis = get_redis_connection("default")
+            redis.ping()  # Отправляем PING-запрос
+            self.stdout.write(self.style.SUCCESS("Redis is available."))
+        except Exception as e:
+            self.stderr.write(self.style.ERROR(f"Redis is not available: {e}"))
+            self.stderr.write("Stopping bot startup.")
+            exit(1)  # Прерываем выполнение
 
     def handle(self, *args, **options):
+
+        # Проверяем Redis только если USE_DOCKER == "0"
+        if USE_DOCKER == "0":
+            self.check_redis()
+
         try:
             # Запускаем бота через asyncio.run
             asyncio.run(start_bot())
